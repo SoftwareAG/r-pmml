a870f8ae4ae69c928cdecef7f30d3eaccb123d59
dbolotov <dmitriy.bolotov@softwareag.com>
2019-05-30 17:36:23 -0700

    applied styler

--- R/xform_wrap.R
+++ R/xform_wrap.R
@@ -1,17 +1,17 @@
 # PMML: Predictive Model Markup Language
-# 
+#
 # Copyright (c) 2009-2016, Zementis, Inc.
-# Copyright (c) 2016-2019, Software AG, Darmstadt, Germany and/or Software AG 
-# USA Inc., Reston, VA, USA, and/or its subsidiaries and/or its affiliates 
+# Copyright (c) 2016-2019, Software AG, Darmstadt, Germany and/or Software AG
+# USA Inc., Reston, VA, USA, and/or its subsidiaries and/or its affiliates
 # and/or their licensors.
-# 
+#
 # This file is part of the PMML package for R.
-# 
-# The PMML package is free software: you can redistribute it and/or 
+#
+# The PMML package is free software: you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
-# published by the Free Software Foundation, either version 3 of 
+# published by the Free Software Foundation, either version 3 of
 # the License, or (at your option) any later version.
-# 
+#
 # The PMML package is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. Please see the
