778140c79a5adddd788124ffb188f09e85fb0bc2
dbolotov <dmitriy.bolotov@softwareag.com>
2020-04-17 13:20:46 -0700

    removed skip_on_ci()

--- tests/testthat/test_pmml.iForest.R
+++ tests/testthat/test_pmml.iForest.R
@@ -1,10 +1,10 @@
+library(isofor)
 data(iris)
 
 test_that("confirm non-existent category is still automatically created by iForest function", {
   skip_on_cran()
-  skip_on_ci()
+  # skip_on_ci()
 
-  library(isofor)
   mod <- iForest(iris, nt = 2, phi = 30)
   model_pmml <- pmml(model = mod)
   expect_equal(length(model_pmml[[2]]), 5)
