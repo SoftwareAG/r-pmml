441e608575e1046e4fa090ed3feae42419ebd8aa
dbolotov <dmitriy.bolotov@softwareag.com>
2021-01-28 18:23:29 -0800

    moved xform_function tests to separate file

--- tests/testthat/test_transformations.R
+++ tests/testthat/test_transformations.R
@@ -161,37 +161,6 @@ test_that("xform_map produces correct mapping for a data point", {
   expect_true(audit_box$data$d_sex[[1]] == 3)
 })
 
-test_that("xform_function works correctly", {
-  iris_box <- xform_wrap(iris)
-  iris_box <- xform_function(iris_box,
-    orig_field_name = "Sepal.Length",
-    new_field_name = "Sepal.Length.Transformed",
-    expression = "(Sepal.Length^2)/100"
-  )
-  expect_equal(iris_box$field_data["Sepal.Length.Transformed", "orig_field_name"], "Sepal.Length")
-  expect_equal(iris_box$field_data["Sepal.Length.Transformed", "xform_function"], "(Sepal.Length^2)/100")
-  expect_equal(iris_box$data$Sepal.Length.Transformed[[1]], 0.2601)
-})
-
-test_that("xform_function does not create an unnecessary NA factor", {
-  iris_box_xf <- xform_wrap(iris)
-  iris_box_xf <- xform_function(iris_box_xf,
-    orig_field_name = "Sepal.Width",
-    new_field_name = "Sepal.Width.sq",
-    expression = "Sepal.Width^2",
-    new_field_data_type = "numeric"
-  )
-  expect_equal(levels(iris_box_xf$field_data$dataType), c("factor", "numeric"))
-
-  iris_box_xf_2 <- xform_wrap(iris)
-  iris_box_xf_2 <- xform_function(iris_box_xf_2,
-    orig_field_name = "Sepal.Length,Petal.Length",
-    new_field_name = "Length.Ratio",
-    expression = "Sepal.Length / Petal.Length"
-  )
-  expect_equal(levels(iris_box_xf_2$field_data$dataType), c("factor", "numeric"))
-})
-
 
 
 test_that("xform_discretize does not give error when 1st column of data matrix is a factor", {
